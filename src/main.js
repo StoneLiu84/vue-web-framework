24ba7d10b9becc84747dfa497d4e34ce30459047
StoneLiu84 <liaoyqiang@foxmail.com>
2019-09-03 15:49:25 +0800

    new project

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'vx-easyui/dist/themes/bootstrap/easyui.css'
+import 'vx-easyui/dist/themes/icon.css'
+import 'vx-easyui/dist/themes/vue.css'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import EasyUI from 'vx-easyui'
+
+Vue.use(EasyUI)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
